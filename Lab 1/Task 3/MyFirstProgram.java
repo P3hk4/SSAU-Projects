d07298037e969866586abdc5a7fd2a26f478cbfc
Pavel Tarasov <pavel44tarasov@yandex.ru>
2023-09-22 19:03:45 +0400

    Task 3 commit

---
+++ Lab 1/Task 3/MyFirstProgram.java
@@ -0,0 +1,45 @@
+class MyFirstClass{
+	public static void main(String [] s){
+	MySecondClass mySecondClass = new MySecondClass(42,10);
+	System.out.println(mySecondClass.getMinimumNumber());
+	for (int i = 1; i <= 8; i++){
+		for (int j = 1; j <= 8; j++){
+			mySecondClass.setFrstNum(i);
+			mySecondClass.setScndNum(j);
+			System.out.println(mySecondClass.getMinimumNumber());
+			System.out.println("\n");
+		}
+		System.out.println();
+	}
+}
+}
+class MySecondClass{
+	private int frstNum = 0;
+	private int scndNum = 0;
+
+	public int getMinimumNumber(){
+		if(getFrstNum()<=getScndNum()) return getFrstNum();
+		else return getScndNum();
+	}
+
+	public int getFrstNum() {
+		return frstNum;
+	}
+
+	public int getScndNum() {
+		return scndNum;
+	}
+
+	public void setFrstNum(int frstNum) {
+		this.frstNum = frstNum;
+	}
+
+	public void setScndNum(int scndNum) {
+		this.scndNum = scndNum;
+	}
+
+	public MySecondClass(int frstNum, int scndNum) {
+		this.frstNum = frstNum;
+		this.scndNum = scndNum;
+	}
+}
